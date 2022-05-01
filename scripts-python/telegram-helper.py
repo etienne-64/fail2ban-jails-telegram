558b8d11a81026e338f24a32ec85498d885caa4e
svogelsinger <svogelsinger@pm.me>
2022-05-01 20:34:27 +0200

    skeleton

--- scripts-python/telegram-helper.py
+++ scripts-python/telegram-helper.py
@@ -1,3 +1,30 @@
 #!/usr/bin/env python3
 
-# ./install.sh python 
+import subprocess
+from datetime import datetime
+from socket import gethostname, gethostbyname
+
+def send_to_telegram(message):
+    status = False
+
+    # Get Telegram id & key
+    id = subprocess.run('cat telegram.id', shell=True, capture_output=True).stdout.decode('utf-8')
+    key = subprocess.run('cat telegram.key', shell=True, capture_output=True).stdout.decode('utf-8')
+    # Get & format current date
+    now = datetime.now().strftime("%d/%m/%Y %H:%M")
+
+    hostname = gethostname()
+    ip = gethostbyname(gethostname())
+
+    message = f"{now} - {hostname} \n {message}"
+
+    return status
+
+
+if __name__ == "__main__":
+
+    try:
+        pass
+
+    except Exception as err:
+        print(f"Erreur: {err}")
