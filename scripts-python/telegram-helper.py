1ede43c14089cb30d8c01502b889c79ff6cafbb5
svogelsinger <svogelsinger@pm.me>
2022-05-02 18:46:37 +0200

    send_to_telegram. Tested

--- scripts-python/telegram-helper.py
+++ scripts-python/telegram-helper.py
@@ -2,29 +2,36 @@
 
 import subprocess
 from datetime import datetime
-from socket import gethostname, gethostbyname
+from socket import gethostname
+import requests
+import argparse
 
 def send_to_telegram(message):
-    status = False
-
     # Get Telegram id & key
     id = subprocess.run('cat telegram.id', shell=True, capture_output=True).stdout.decode('utf-8')
     key = subprocess.run('cat telegram.key', shell=True, capture_output=True).stdout.decode('utf-8')
     # Get & format current date
     now = datetime.now().strftime("%d/%m/%Y %H:%M")
+    url = f"https://api.telegram.org/bot{key}/sendMessage"
 
     hostname = gethostname()
-    ip = gethostbyname(gethostname())
 
-    message = f"{now} - {hostname} \n {message}"
+    message = f"Origine: {now} - {hostname} \n {message}"
 
-    return status
+    data = {'chat_id': id , 'text': message, 'disable_web_preview': 'true', 'parse_mode':'markdown'}
 
+    req = requests.post(url, data=data)
 
 if __name__ == "__main__":
 
+    # Get input choice
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-m","--message", help="Message to be sent", required=True)
+    args = parser.parse_args()
+    message = args.message
+
     try:
-        pass
+        send_to_telegram(message)
 
     except Exception as err:
         print(f"Erreur: {err}")
